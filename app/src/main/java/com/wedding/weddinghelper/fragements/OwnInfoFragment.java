d2c80ec35c4c7006110dce0bcf1e2605815b86fe
志豪 陳 <Neal@zhihaode-MacBook-Pro.local>
2016-08-16 08:13:46 +0800

    取得婚宴資訊並顯示。 完成婚宴資訊頁面UI。

--- app/src/main/java/com/wedding/weddinghelper/fragements/OwnInfoFragment.java
+++ app/src/main/java/com/wedding/weddinghelper/fragements/OwnInfoFragment.java
@@ -9,6 +9,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.wedding.weddinghelper.R;
 
 public class OwnInfoFragment extends Fragment {
@@ -30,12 +35,31 @@ public class OwnInfoFragment extends Fragment {
         return view;
     }
 
+    TextView groomAndBrideName, engageTime, engagePlace, engageAddress, marryTime, marryPlace, marryAddress;
     @Override
-    public void onActivityCreated(Bundle savedInstanceState)
-    {
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        groomAndBrideName = (TextView) getView().findViewById(R.id.text_own_info);
+        engageTime = (TextView) getView().findViewById(R.id.engage_time);
+        engagePlace = (TextView) getView().findViewById(R.id.engage_place);
+        engageAddress = (TextView) getView().findViewById(R.id.engage_address);
+        marryTime = (TextView) getView().findViewById(R.id.marry_time);
+        marryPlace = (TextView) getView().findViewById(R.id.marry_place);
+        marryAddress = (TextView) getView().findViewById(R.id.marry_address);
 
-        TextView mText = (TextView) getView().findViewById(R.id.text_own_info);
-        mText.setText(R.string.wedding_info);
+        final ParseInstallation currentInstallation = ParseInstallation.getCurrentInstallation();
+        ParseQuery query = ParseQuery.getQuery("Information");
+        query.getInBackground("XA6hDoxtXo",new GetCallback<ParseObject>() {//ToDo:暫時使用
+            @Override
+            public void done(ParseObject weddingInformation, ParseException e) {
+                groomAndBrideName.setText(weddingInformation.getString("groomName")+"&"+weddingInformation.getString("brideName"));
+                engageTime.setText(weddingInformation.getString("engageDate"));
+                engagePlace.setText(weddingInformation.getString("engagePlace"));
+                engageAddress.setText(weddingInformation.getString("engageAddress"));
+                marryTime.setText(weddingInformation.getString("marryDate"));
+                marryPlace.setText(weddingInformation.getString("marryPlace"));
+                marryAddress.setText(weddingInformation.getString("marryAddress"));
+            }
+        });
     }
 }
