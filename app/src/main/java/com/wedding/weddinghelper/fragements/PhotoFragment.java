748261ccdc4ed47382aa7e1dcf74f2cae84eada0
志豪 陳 <Neal@zhihaode-MacBook-Pro.local>
2016-09-03 15:07:24 +0800

    使用3rd party的library Picasso負責處理image的loading，可顯示照片列表。

--- app/src/main/java/com/wedding/weddinghelper/fragements/PhotoFragment.java
+++ app/src/main/java/com/wedding/weddinghelper/fragements/PhotoFragment.java
@@ -1,15 +1,38 @@
 package com.wedding.weddinghelper.fragements;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.wedding.weddinghelper.R;
+import com.wedding.weddinghelper.activities.GuestListActivity;
+import com.wedding.weddinghelper.activities.JoinMainActivity;
+import com.wedding.weddinghelper.activities.OwnMainActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class PhotoFragment extends Fragment {
     public static PhotoFragment newInstance() {
@@ -18,22 +41,62 @@ public class PhotoFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    public String weddingInfoObjectId;
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Log.d("Neal","activity class = "+activity.getLocalClassName());
+        if (activity.getLocalClassName().equals("activities.OwnMainActivity")){
+            OwnMainActivity mMainActivity = (OwnMainActivity) activity;
+            weddingInfoObjectId = mMainActivity.getWeddingInfoObjectId();
+        }
+        else {
+            JoinMainActivity mMainActivity = (JoinMainActivity) activity;
+            weddingInfoObjectId = mMainActivity.getWeddingInfoObjectId();
+        }
+        Log.d("Neal", weddingInfoObjectId);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    String [] photoUrls;
+    private GridView photoGridView;
+    private ParseFile[]photos;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo, container, false);
+        photoGridView = (GridView)view.findViewById(R.id.photo_grid_view);
+        ParseQuery query = ParseQuery.getQuery(weddingInfoObjectId+"Photo");
+        query.orderByAscending("Shooter");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(final List<ParseObject> list, ParseException e) {
+                photoUrls = new String[list.size()];
+                for (int i = 0 ; i<list.size() ; i++){
+                    ParseFile photoFile = list.get(i).getParseFile("microPhoto");
+                    photoUrls[i] = photoFile.getUrl();
+                }
+                photoGridView.setAdapter(new gridViewCustomAdapter(getContext(), photoUrls));
+                photoGridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(getContext(), "Your choice is "+position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
         return(view);
     }
-
     @Override
-    public void onActivityCreated(Bundle savedInstanceState)
-    {
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
+
+
+
 }
---
+++ app/src/main/java/com/wedding/weddinghelper/fragements/gridViewCustomAdapter.java
@@ -0,0 +1,56 @@
+package com.wedding.weddinghelper.fragements;
+
+/**
+ * Created by Neal on 2016/9/3.
+ */
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import com.wedding.weddinghelper.R;
+import com.squareup.picasso.Picasso;
+public class gridViewCustomAdapter extends BaseAdapter {
+
+    //int[] data={R.drawable.couple,R.drawable.create_wedding};
+    private Context context;
+
+    private  String[] photoUrls;
+    public gridViewCustomAdapter(Context context, String[] thePhotoUrls){
+        this.context = context;
+        photoUrls = thePhotoUrls;
+    }
+
+    @Override
+    public int getCount() {
+        return photoUrls.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return photoUrls[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView image = (ImageView)convertView;
+        if (image == null) {
+            image = new ImageView(context);
+        }
+        String url = (String) getItem(position);
+
+        Picasso.with(context)
+                .load(url)
+                .resize(200,200)
+                .centerCrop()
+                .into(image);
+        return image;
+    }
+}
