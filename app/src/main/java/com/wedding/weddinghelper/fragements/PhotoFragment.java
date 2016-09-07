a5a60bf8c15143c6d54e0d9d676d452eec5bd2bd
志豪 陳 <Neal@zhihaode-MacBook-Pro.local>
2016-09-07 08:10:04 +0800

    上傳照片功能未完成，因此先顯示“功能尚未開放”的提示訊息。

--- app/src/main/java/com/wedding/weddinghelper/fragements/PhotoFragment.java
+++ app/src/main/java/com/wedding/weddinghelper/fragements/PhotoFragment.java
@@ -2,8 +2,14 @@ package com.wedding.weddinghelper.fragements;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,6 +31,13 @@ import com.wedding.weddinghelper.activities.JoinMainActivity;
 import com.wedding.weddinghelper.activities.OwnMainActivity;
 import com.wedding.weddinghelper.activities.PhotoViewActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
@@ -66,14 +79,64 @@ public class PhotoFragment extends Fragment {
         int id = item.getItemId();
         if(id == R.id.action_camera){
             //ToDo:使用相機拍照，並上傳照片
+            //dispatchTakePictureIntent();
+            new AlertDialog.Builder(getContext())
+                    .setTitle("訊息")
+                    .setMessage("功能尚未開放呦！")
+                    .setPositiveButton("好！", null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
             return true;
         }
         else if(id == R.id.action_album){
             //ToDo:從相簿裡選擇照片，並上傳照片
+            new AlertDialog.Builder(getContext())
+                    .setTitle("訊息")
+                    .setMessage("功能尚未開放呦！")
+                    .setPositiveButton("好！", null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+    /*
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(), System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    */
 
     private GridView photoGridView;
     private ParseFile[]photos;
